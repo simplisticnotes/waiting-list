abd22025679f63c8f48ecee8f91c4b1e092036f6
Shahmir Faisal <shahmir049@gmail.com>
2022-11-30 00:07:33 +0500

    finished

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,60 @@
 import Head from "next/head";
 import React from "react";
+import classes from "../styles/home.module.scss";
 
-function index() {
+function HomePage() {
   return (
     <>
       <Head>
         <title>Simplistic Notes - Write Anything, Write Securely</title>
       </Head>
+
+      <section className={classes.topContainer}>
+        <header>
+          <img src="/logo.svg" alt="Simplistic Notes Logo" />
+          <h3>implistic Notes</h3>
+        </header>
+
+        <section className={classes.hero}>
+          <h1>Write anything. Write securely.</h1>
+          <p>
+            Simplistic Notes is a private, secure, and simple way to take notes.
+            Write anything to help you remember later without the worry of
+            someone else reading your notes.
+          </p>
+
+          <form>
+            <input type="email" placeholder="Email..." required />
+            <button>Join Waiting List</button>
+          </form>
+          <p className={classes.formCaption}>
+            Get early access to the software
+          </p>
+        </section>
+      </section>
+
+      <section className={classes.featuresContainer}>
+        <h2>Features</h2>
+
+        <section className={classes.features}>
+          <div>
+            <img src="/lock.svg" alt="End-to-End Encryption Icon" />
+            <p>End-to-end Encryption</p>
+          </div>
+
+          <div>
+            <img src="/notebook.svg" alt="Rich Text, Todos and Markdown Icon" />
+            <p>Rich Text, Todos, Markdown</p>
+          </div>
+
+          <div>
+            <img src="/folder.svg" alt="Organize Notes in Folders Icon" />
+            <p>Organize in Folders and Subfolders</p>
+          </div>
+        </section>
+      </section>
     </>
   );
 }
 
-export default index;
+export default HomePage;
